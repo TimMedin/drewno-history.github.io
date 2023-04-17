ba7f7a0eab7659b35ab17e08ac5b559e9338e8e7
TimMedin <timmedin180@gmail.com>
2023-04-17 23:48:04 +0200

    update html, css, change photo

---
+++ index.js
@@ -0,0 +1,9 @@
+  const buttons = document.querySelectorAll(".read-more-button");
+
+  buttons.forEach(button => {
+    const hiddenText = button.previousElementSibling;
+    button.addEventListener("click", () => {
+      hiddenText.classList.toggle("hidden");
+      button.textContent = hiddenText.classList.contains("hidden") ? "Читати більше" : "Згорнути";
+    });
+  });
